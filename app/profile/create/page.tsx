5849be10604a9c30334e339f36236041d5a86452
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-12-01 00:24:32 +0100

    feat(profile): Add CreateProfilePage component with form submission

---
+++ app/profile/create/page.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+
+import React from "react";
+
+async function createProfileAction(formData: FormData) {
+  "use server";
+  const firstName = formData.get("firstName") as string;
+  console.log(firstName);
+}
+async function CreateProfilePage() {
+  return (
+    <section>
+      <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
+      <div className='border p-8 rounded-md max-w-lg'>
+        <form action={createProfileAction}>
+          <div className='mb-2'></div>
+          <Button type='submit' size='lg'>
+            Create Profile
+          </Button>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default CreateProfilePage;
