14365380fb3308f3bcc50bb865d407584f6618c2
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-12-02 23:04:26 +0100

    feat(schemas): enhance profileSchema with custom error messages for validation

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -2,7 +2,13 @@ import * as z from "zod";
 // import { ZodSchema } from "zod";
 
 export const profileSchema = z.object({
-  firstName: z.string().min(2),
-  lastName: z.string().min(2),
-  userName: z.string().min(2),
+  firstName: z
+    .string()
+    .min(2, { message: "First name should be at least 2 characters" }),
+  lastName: z
+    .string()
+    .min(2, { message: "Last name should be at least 2 characters" }),
+  userName: z
+    .string()
+    .min(2, { message: "User name should be at least 2 characters" }),
 });
