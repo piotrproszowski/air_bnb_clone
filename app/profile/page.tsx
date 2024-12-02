c3bba0c794457a3968c02bc548d41229bd89ffc6
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-12-02 23:04:34 +0100

    feat(profile): implement ProfilePage with profile fetching and update functionality

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { SubmitButton } from "@/components/form/Buttons";
+import FormContainer from "@/components/form/FormContainer";
+import FormInput from "@/components/form/FormInput";
+import { fetchProfile, updateProfileAction } from "@/utils/actions";
 
-function ProfilePage() {
-  return <h1 className='text-3xl'>ProfilePage</h1>;
+async function ProfilePage() {
+  const profile = await fetchProfile();
+
+  return (
+    <section>
+      <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
+      <div className='border p-8 rounded-md'>
+        <FormContainer action={updateProfileAction}>
+          <div className='grid md:grid-cols-2 gap-4 mt-4'>
+            <FormInput
+              type='text'
+              name='firstName'
+              label='First Name'
+              defaultValue={profile.firstName}
+            />
+            <FormInput
+              type='text'
+              name='lastName'
+              label='Last Name'
+              defaultValue={profile.lastName}
+            />
+            <FormInput
+              type='text'
+              name='userName'
+              label='User Name'
+              defaultValue={profile.userName}
+            />
+          </div>
+          <SubmitButton text='Update Profile' className='mt-8' />
+        </FormContainer>
+      </div>
+    </section>
+  );
 }
 
 export default ProfilePage;
