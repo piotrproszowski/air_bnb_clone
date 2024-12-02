12984cb471143eb76b16be6981117fbaa816b9db
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-12-02 18:09:19 +0100

    feat(navbar): update UserIcon to fetch and display user profile image

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,7 +1,20 @@
+import { fetchProfileImage } from "@/utils/actions";
 import { LuUser2 } from "react-icons/lu";
+import Image from "next/image";
 
-const UserIcon = () => {
-  return <LuUser2 className='w-6 h-6 bg-primary rounded-full text-white' />;
+const UserIcon = async () => {
+  const profileImage = await fetchProfileImage();
+  return profileImage ? (
+    <Image
+      src={profileImage}
+      alt='User profile image'
+      width={24}
+      height={24}
+      className='w-6 h-6 rounded-full object-cover'
+    />
+  ) : (
+    <LuUser2 className='w-6 h-6 bg-primary rounded-full text-white' />
+  );
 };
 
 export default UserIcon;
