ee7e5a0eb760f0ae001546505e5f27e609b46641
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-12-02 17:50:11 +0100

    feat(profile): implement createProfileAction and fetchProfileImage functions for profile management

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,18 +1,48 @@
 "use server";
 
 import { profileSchema } from "./schemas";
+import db from "./db";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export async function createProfileAction(
   prevState: unknown,
   formData: FormData,
 ) {
   try {
+    const user = await currentUser();
+    if (!user) throw new Error("Please login to create a profile");
+
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
-    console.log(validatedFields);
-    return { message: "Profile created successfully" };
+
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? "",
+        ...validatedFields,
+      },
+    });
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: { hasProfile: true },
+    });
   } catch (error) {
-    console.error(error);
-    return { message: `There was an error: ${error}` };
+    return {
+      message: error instanceof Error ? error.message : "An error occurred",
+    };
   }
+  redirect("/");
 }
+
+export const fetchProfileImage = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const profile = await db.profile.findUnique({
+    where: { clerkId: user.id },
+    select: { profileImage: true },
+  });
+
+  return profile?.profileImage;
+};
