a7a00a755dbb3f8afac67337548b59d9d1605925
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-12-10 01:28:01 +0100

    feat(home): implement CategoriesList and PropertiesContainer components; update HomePage to utilize search parameters

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
+import CategoriesList from "@/components/home/CategoriesList";
+import PropertiesContainer from "@/components/home/PropertiesContainer";
 import { Button } from "@/components/ui/button";
 import React from "react";
 
-function HomePage() {
+interface SearchParamsType {
+  searchParams: {
+    category?: string;
+    search?: string;
+  };
+}
+
+function HomePage({ searchParams }: SearchParamsType) {
   return (
-    <div>
-      <h1 className='text-3xl'>HomePage</h1>
-      <Button variant={"outline"} size={"lg"} className='capitalize m-8'>
-        Click me
-      </Button>
-    </div>
+    <section>
+      <CategoriesList
+        category={searchParams.category}
+        search={searchParams.search}
+      />
+      <PropertiesContainer
+        category={searchParams.category}
+        search={searchParams.search}
+      />
+    </section>
   );
 }
 
