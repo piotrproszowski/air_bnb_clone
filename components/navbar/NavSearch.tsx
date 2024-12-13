d093a7eebe32cf3532a2a275ad0dec4c5290f05d
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-12-13 23:16:55 +0100

    feat(navbar): enhance NavSearch component with debounced search functionality and URL parameter handling

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,12 +1,43 @@
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 import { Input } from "../ui/input";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const NavSearch = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const [search, setSearch] = useState(
+    searchParams.get("search")?.toString() || "",
+  );
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 500);
+
+  useEffect(() => {
+    if (!searchParams) {
+      setSearch("");
+    }
+  }, [searchParams.get("search")]);
+
   return (
     <Input
       type='text'
       placeholder='Search'
       className='max-w-xs dark:bg-muted'
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
+      value={search}
     />
   );
 };
