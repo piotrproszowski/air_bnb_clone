13a000d7c5533f4418c2f7287671674d5ce7572d
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2025-01-18 21:24:43 +0100

    refactor(FavoriteToggleButton): replace fetchFavoriteId with Actions.fetchFavoriteId; ensure favoriteId is a string in FavoriteToggleForm

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,13 +1,15 @@
 import { CardSignInButton } from "../form/Buttons";
 import { auth } from "@clerk/nextjs/server";
-import { fetchFavoriteId } from "@/utils/actions";
+import { Actions } from "@/utils/actions";
 import FavoriteToggleForm from "./FavoriteToggleForm";
 
 const FavoriteToggleButton = async ({ propertyId }: { propertyId: string }) => {
   const { userId } = auth();
   if (!userId) return <CardSignInButton />;
-  const favoriteId = await fetchFavoriteId({ propertyId });
-  return <FavoriteToggleForm favoriteId={favoriteId} propertyId={propertyId} />;
+  const favoriteId = await Actions.fetchFavoriteId({ propertyId });
+  return (
+    <FavoriteToggleForm favoriteId={favoriteId || ""} propertyId={propertyId} />
+  );
 };
 
 export default FavoriteToggleButton;
